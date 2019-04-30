ca11ba4b27e7b007fcc6c92dfca576d6634eabcb
rbrbrb7290 <ohg429@gmail.com>
2019-04-30 22:21:43 +0900

    Refactory showAlert()

--- app/src/main/java/capstone/son/cut/fundingapp/Login_API2.java
+++ app/src/main/java/capstone/son/cut/fundingapp/Login_API2.java
@@ -26,26 +26,33 @@ import java.util.concurrent.ExecutionException;
 public class Login_API2 extends AppCompatActivity {
     private TextView tv_outPut;
 
+    //TODO form에서 입력했던 사용자 정보들을 POST로 보내기
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__api2);
         // 위젯에 대한 참조.
-         tv_outPut = (TextView) findViewById(R.id.tv_outPut);
-       findViewById(R.id.nextBtn).setOnClickListener(goMain);
+        tv_outPut = (TextView) findViewById(R.id.tv_outPut);
+        findViewById(R.id.nextBtn).setOnClickListener(goMain);
 
         Intent prevIntent = getIntent();
         final HashMap<String, String> extra = (HashMap<String, String>) prevIntent.getSerializableExtra("data");
-        try {
-            new HttpConnectionThread().execute(extra).get();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        DownLoadTask downLoadTask = new DownLoadTask();
+
+        Integer[ ] arr = new Integer[3];
+        downLoadTask.execute(arr);
+        downLoadTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, arr);
+//        try {
+//            new HttpConnectionThread().execute(extra).get();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         // URL 설정.
-       // String url = "http://192.168.0.20:8080";
+        // String url = "http://192.168.0.20:8080";
 
         // AsyncTask를 통해 HttpURLConnection 수행.
 //        NetworkTask networkTask = new NetworkTask(url, null);
@@ -76,66 +83,98 @@ public class Login_API2 extends AppCompatActivity {
     Button.OnClickListener goMain = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent=new Intent(Login_API2.this,MainActivity.class);
+            Intent intent = new Intent(Login_API2.this, MainActivity.class);
             startActivity(intent);
         }
     };
 
-    public class HttpConnectionThread  extends AsyncTask<Object ,String ,String>{
+    private class DownLoadTask extends AsyncTask<Integer, Integer, String>{
 
-        @Override
-        protected String doInBackground(Object... strings) {
-            Log.i("test", "시작");
-            HashMap<String , String> extra = (HashMap<String, String>) strings[0];
-            JSONObject BODY = new JSONObject(extra);
+        private String s;
 
-            URL url;
-            String response = null;
-            try {
-                url = new URL("http://117.17.102.139:8080/addAccount");
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(10000 /* milliseconds */);
-                conn.setConnectTimeout(15000 /* milliseconds */);
-
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Accept-Charset", "UTF-8"); // Accept-Charset ����. //character set을 utf-8로 선언
-                conn.setRequestProperty("Content-Type", "application/json");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                conn.connect();
-                response = conn.getResponseMessage();
-                Log.d("RESPONSE", "The response is: " + response);
-
-                OutputStream OutStream = conn.getOutputStream();
-                OutStream.write(BODY.toString().getBytes("utf-8"));//Body는 서버로보낼 스트링값등을 설정하는 것
-
-                int resCode = conn.getResponseCode(); // connect, send http reuqest, receive htttp request
-                System.out.println ("code = "+ resCode);
-
-                InputStreamReader InputStream = new InputStreamReader(conn.getInputStream(), "UTF-8");//InputStreamReader는 서버로부터 안드로이드로 받아오는 데이터 흐름을 읽어주는 클래스
-                BufferedReader Reader = new BufferedReader(InputStream);
-                StringBuilder Builder = new StringBuilder();//스트링을 만들어주는데 유용하게쓰이는 클래스
-                String ResultStr; //저장할 공간
-
-                while ((ResultStr = Reader.readLine()) != null) {//(중요)서버로부터 한줄씩 읽어서 문자가 없을때까지 넣어줌
-                    System.out.println(ResultStr);
-                    Builder.append(ResultStr + "\n"); //읽어준 스트링값을 더해준다.
-                }
-
-                Log.i("gg", Builder.toString());
-                response = Builder.toString();
-            }
-            catch (IOException e) {
-            }
-            return response;
-         }
-        @Override
-        protected void onPostExecute(String result){
-            Toast.makeText(getApplicationContext(),result, Toast.LENGTH_SHORT).show();
-
+        @Override //작업 실행전 메인쓰레드에서 호출(진행률 표지줄을 표시하는데 씀)
+        protected void onPreExecute() {
+            super.onPreExecute();
         }
 
+        @Override
+        protected String doInBackground(Integer... integers) {
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected  void onCancelled(String result){
+            super.onCancelled(result);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer ... values){
+            super.onProgressUpdate();
+        }
+
+
     }
+}
+
+//    public class HttpConnectionThread  extends AsyncTask<Object ,String ,String>{
+//
+//        @Override
+//        protected String doInBackground(Object... strings) {
+//            Log.i("test", "시작");
+//            HashMap<String , String> extra = (HashMap<String, String>) strings[0];
+//            JSONObject BODY = new JSONObject(extra);
+//
+//            URL url;
+//            String response = null;
+//            try {
+//                url = new URL("http://117.17.102.139:8080/addAccount");
+//                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                conn.setReadTimeout(10000 /* milliseconds */);
+//                conn.setConnectTimeout(15000 /* milliseconds */);
+//
+//                conn.setRequestMethod("POST");
+//                conn.setRequestProperty("Accept-Charset", "UTF-8"); // Accept-Charset ����. //character set을 utf-8로 선언
+//                conn.setRequestProperty("Content-Type", "application/json");
+//                conn.setDoInput(true);
+//                conn.setDoOutput(true);
+//                conn.connect();
+//                response = conn.getResponseMessage();
+//                Log.d("RESPONSE", "The response is: " + response);
+//
+//                OutputStream OutStream = conn.getOutputStream();
+//                OutStream.write(BODY.toString().getBytes("utf-8"));//Body는 서버로보낼 스트링값등을 설정하는 것
+//
+//                int resCode = conn.getResponseCode(); // connect, send http reuqest, receive htttp request
+//                System.out.println ("code = "+ resCode);
+//
+//                InputStreamReader InputStream = new InputStreamReader(conn.getInputStream(), "UTF-8");//InputStreamReader는 서버로부터 안드로이드로 받아오는 데이터 흐름을 읽어주는 클래스
+//                BufferedReader Reader = new BufferedReader(InputStream);
+//                StringBuilder Builder = new StringBuilder();//스트링을 만들어주는데 유용하게쓰이는 클래스
+//                String ResultStr; //저장할 공간
+//
+//                while ((ResultStr = Reader.readLine()) != null) {//(중요)서버로부터 한줄씩 읽어서 문자가 없을때까지 넣어줌
+//                    System.out.println(ResultStr);
+//                    Builder.append(ResultStr + "\n"); //읽어준 스트링값을 더해준다.
+//                }
+//
+//                Log.i("gg", Builder.toString());
+//                response = Builder.toString();
+//            }
+//            catch (IOException e) {
+//            }
+//            return response;
+//         }
+//        @Override
+//        protected void onPostExecute(String result){
+//            Toast.makeText(getApplicationContext(),result, Toast.LENGTH_SHORT).show();
+//
+//        }
+//
+//    }
 
 
 //    public class NetworkTask extends AsyncTask<Void, Void, String> {
@@ -166,4 +205,4 @@ public class Login_API2 extends AppCompatActivity {
 //            tv_outPut.setText(s);
 //        }
 //    }
-}
+
--- app/src/main/java/capstone/son/cut/fundingapp/Login_Activity.java
+++ app/src/main/java/capstone/son/cut/fundingapp/Login_Activity.java
@@ -42,38 +42,46 @@ public class Login_Activity extends AppCompatActivity {
         password = et_pw.getText().toString();
         password_chk = et_pw_chk.getText().toString();
 
-        if(password.equals(password_chk))
-        {
-            /* 패스워드 확인이 정상적으로 됨 */
-            HashMap<String, String> extra = new HashMap<>();
-            extra.put("name", name);
-            extra.put("birth",  birthgender);
-            extra.put("sex", sex);
-            extra.put("email",email);
-            extra.put("id",id);
-            extra.put("password",  password);
+        if(!password.equals("") ){
+            if(password.equals(password_chk)) {
+                /* 패스워드 확인이 정상적으로 됨 */
+                HashMap<String, String> extra = new HashMap<>();
+                extra.put("name", name);
+                extra.put("birth",  birthgender);
+                extra.put("sex", sex);
+                extra.put("email",email);
+                extra.put("id",id);
+                extra.put("password",  password);
 
-            Intent intent=new Intent(Login_Activity.this,Login_API.class);
-            intent.putExtra("data", extra);
-            startActivity(intent);
-
+                Intent intent=new Intent(Login_Activity.this,Login_API.class);
+                intent.putExtra("data", extra);
+                startActivity(intent);
+            }
+            else {
+                AlertDialog.Builder alert = showAlert();
+                alert.setMessage("비밀번호를 확인해주십시오.");
+                alert.show();
+            }
         }
-        else
-        {
-            AlertDialog.Builder alert = new AlertDialog.Builder(Login_Activity.this);
-            alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();     //닫기
-                }
-            });
-            alert.setMessage("비번확인좀");
+        else {
+            AlertDialog.Builder alert = showAlert();
+            alert.setMessage("비밀번호를 입력해주십시오.");
             alert.show();
-
         }
+
+
     }
 
-
+    private AlertDialog.Builder showAlert() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(Login_Activity.this);
+        alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();     //닫기
+            }
+        });
+        return alert;
+    }
 
 
 }
